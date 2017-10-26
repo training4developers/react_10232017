1198b32aeb115800f6f7c97afcac4570ecbd0bb6
Eric W. Greene <eric@ericwgreene.com>
2017-10-26 08:17:06 -0700

    added code demos

--- client/js/components/car-view-row.js
+++ client/js/components/car-view-row.js
@@ -1,18 +1,28 @@
 import * as React from 'react';
 
-export class CarViewRow extends React.Component {
+export const CarViewRow = props => <tr>
+  <td>{props.car.make}</td>
+  <td>{props.car.model}</td>
+  <td>{props.car.year}</td>
+  <td>{props.car.color}</td>
+  <td>{props.car.price}</td>
+  <td>
+    <button type="button" onClick={ () => props.onDelete(props.car.id) }>Delete</button>
+  </td>
+</tr>;
 
-
-  render() {
-    return <tr>
-      <td>{this.props.car.make}</td>
-      <td>{this.props.car.model}</td>
-      <td>{this.props.car.year}</td>
-      <td>{this.props.car.color}</td>
-      <td>{this.props.car.price}</td>
-      <td>
-        <button type="button" onClick={ () => this.props.onDelete(this.props.car.id) }>Delete</button>
-      </td>
-    </tr>;
-  }
-}
+// export const CarViewRow = props => {
+  
+//     const deleteCar = () => props.onDelete(props.car.id);
+  
+//     return <tr>
+//       <td>{props.car.make}</td>
+//       <td>{props.car.model}</td>
+//       <td>{props.car.year}</td>
+//       <td>{props.car.color}</td>
+//       <td>{props.car.price}</td>
+//       <td>
+//         <button type="button" onClick={deleteCar}>Delete</button>
+//       </td>
+//     </tr>;
+//   };
